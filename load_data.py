49bc222807ada7af03512df2dee6a00f836f6545
ChanduRayanki <c429702@gmail.com>
2024-07-01 19:37:06 -0400

    Built the CNN model to detect Hand sign (Alphabets)

--- load_data.py
+++ load_data.py
@@ -3,10 +3,10 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 from sklearn.model_selection import train_test_split
+import cv2
+import matplotlib.pyplot as plt
 
 
-
-data_dir = r"E:\CHANDU\Projects\ComputerVisionProjects\SignLangDetect\asl_dataset"
 def load_data(data_dir):
     # load data
     print("Loading data....")
@@ -18,14 +18,9 @@ def load_data(data_dir):
         label_dir = os.path.join(data_dir,label)
         for img in os.listdir(label_dir):
             img_path = os.path.join(label_dir,img)
-            image = load_img(img_path)
-            image = img_to_array(image)
+            image = load_img(img_path,target_size=(32,32,3))
+            #gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
             images.append(image)
-            labels.append(label_map[i])
-    images = np.array(images)
-    labels = np.array(labels)
-    print(labels)
+            labels.append(i)
     print("Done...")
-    return images, labels
-
-load_data(data_dir)
+    return np.asarray(images),np.asarray(labels),label_map
